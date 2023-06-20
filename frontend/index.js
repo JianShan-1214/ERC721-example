783eac9169b7f9bb15ee1e8d4d104f87f905fdcc
JianShan-1214 <l059069045@gmail.com>
2023-06-20 14:26:25 +0800

    修改成開盲盒

--- frontend/index.js
+++ frontend/index.js
@@ -2,24 +2,29 @@ const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 import contract from "./contract.mjs";
 
 const vm = Vue.createApp({
-    data(){
+    data() {
         return {
             isConnected: false,
-            account: ""
-        }
+            account: "",
+            tokenId: "", // Add a data property to store the token ID
+        };
     },
     methods: {
-        async requestETH(){
+        async requestETH() {
             const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
             this.account = accounts[0];
             this.isConnected = true;
         },
-        async mint(){
-            await contract.methods.mint().send({
+        async mint() {
+            const result = await contract.methods.mint().send({
                 from: this.account,
-                value: web3.utils.toWei("0.05", 'ether'),
-            })
-        }
+                value: web3.utils.toWei("0.005", 'ether'),
+            });
+            this.tokenId = result.events.Transfer.returnValues.tokenId; // Store the token ID after minting
+        },
+        async flipReveal() {
+            await contract.methods.flipReveal(this.tokenId).send({ from: this.account });
+        },
     }
 });
 
